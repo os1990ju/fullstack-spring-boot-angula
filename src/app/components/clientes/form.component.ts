801a7e89f386bec2fb9233783ed0f13c89f775a1
oscar.jurado <oscar.jurado@arkangroupco.com>
2023-03-17 00:32:58 -0500

    se agrega la funcionalidad para guardar un cliente

--- src/app/components/clientes/form.component.ts
+++ src/app/components/clientes/form.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+
   public titulo:string = "Formulario de registro";
   public cliente: Cliente = new Cliente();
+  
+  constructor(private clienteservice:ClienteService,
+              private router:Router){}
+
+
+  public create():void{
+    this.clienteservice.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
+    
+  }
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
 import { Cliente } from '../components/clientes/cliente';
 //conexion a server
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  private urlEndpoint:string = 'http://localhost:8080/api/clientes'
+  
+  private urlEndpoint:string = 'http://localhost:8080/api/clientes';
+  //definir cabeceras
+  private httpHeaders = new HttpHeaders({'content-type':'application/json'});
+  //consultar clientes
   getClientes():Observable<Cliente[]>{
     //return of(CLIENTES);//se convierte en un flujo de datos stream para el observable
     return this.http.get<Cliente[]>(this.urlEndpoint)
+  }
+
+  //crear cliente
+  create(cliente:Cliente):Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeaders})
   }
   
   constructor(private http:HttpClient) { 
